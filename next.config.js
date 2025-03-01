1c6f2d9fbb82e734a51409205e85b589e86d1bce
TK <tk@lackner.kabas.at>
2025-03-01 19:01:09 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
-import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import Link from 'next/link';
+import { promises as fs } from 'fs';
 
 interface PostFrontMatter {
   title: string;
@@ -17,23 +17,23 @@ interface Post {
 async function generateBlogList(): Promise<Post[]> {
   try {
     const postsDirectory = path.join(process.cwd(), 'src', 'posts');
-    const filenames = fs.readdirSync(postsDirectory);
-    
-    const posts = filenames.map(filename => {
-      const slug = filename.replace(/\.mdx$/, '');
-      
-      const fullPath = path.join(postsDirectory, filename);
-      const fileContents = fs.readFileSync(fullPath, 'utf8');
-      
-      const { data: frontMatter } = matter(fileContents);
-      
-      return {
-        slug,
-        frontMatter: frontMatter as PostFrontMatter
-      };
-    });
-    
-    // Sort posts by date
+    const filenames = await fs.readdir(postsDirectory);
+
+    // Process each post asynchronously
+    const posts: Post[] = await Promise.all(
+      filenames.map(async (filename) => {
+        const slug = filename.replace(/\.mdx$/, '');
+        const fullPath = path.join(postsDirectory, filename);
+        const fileContents = await fs.readFile(fullPath, 'utf8');
+        const { data: frontMatter } = matter(fileContents);
+        return {
+          slug,
+          frontMatter: frontMatter as PostFrontMatter,
+        };
+      })
+    );
+
+    // Sort posts by date (descending)
     return posts.sort((a, b) => {
       if (a.frontMatter.date < b.frontMatter.date) {
         return 1;
@@ -49,7 +49,7 @@ async function generateBlogList(): Promise<Post[]> {
 
 export default async function Blog() {
   const posts = await generateBlogList();
-  
+
   return (
     <div className="blog-container">
       <h1>Blog</h1>
